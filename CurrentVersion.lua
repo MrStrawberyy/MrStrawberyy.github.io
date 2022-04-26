7b43ea88377015427da11046d3d2ad0e9a046754
MrStrawberyy <88820637+MrStrawberyy@users.noreply.github.com>
2022-04-26 04:29:48 -0400

    Update CurrentVersion.lua

--- CurrentVersion.lua
+++ CurrentVersion.lua
@@ -1,1 +1,11 @@
-local LastestVersion.Test = 1.3
+local LastestVersion = 1.3
+
+function TestVersion(Version)
+   if Version ~= LastestVersion then
+       game.StarterGui:SetCore("SendNotification", {
+          Title = "Version: "..CurrentVersion;
+          Text = "You are Running the Newest Version of Bestieee!";
+          Duration = 5
+       })
+   end
+end
