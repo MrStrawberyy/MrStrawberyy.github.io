2ae452429e2b0997552395f99b7398371120da41
MrStrawberyy <88820637+MrStrawberyy@users.noreply.github.com>
2022-04-26 04:50:11 -0400

    Update CurrentVersion.lua

--- CurrentVersion.lua
+++ CurrentVersion.lua
@@ -1,11 +1,11 @@
 local LastestVersion = 1.3
 
-function TestVersion(Version)
-   if Version ~= LastestVersion then
+pcall(function(Version)
+if Version ~= LastestVersion then
        game.StarterGui:SetCore("SendNotification", {
           Title = "Version: "..Version;
           Text = "You are not running the Lastest Version of BestEEE";
           Duration = 5
        })
    end
-end
+end)
